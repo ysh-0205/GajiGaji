7c535209732f22f5e8693eec537d431834e5fb7e
ysh-0205 <ysh_0205@naver.com>
2025-08-08 10:52:46 +0900

    Update asgi.py

--- Gaji/asgi.py
+++ Gaji/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GajiGaji.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gaji.settings')
 
 application = get_asgi_application()
