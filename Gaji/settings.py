9056c54a418a7d746220459a4aaea0e118f0c5e5
ysh-0205 <ysh_0205@naver.com>
2025-08-08 10:32:26 +0900

    GajiGaji

--- Gaji/settings.py
+++ Gaji/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Movie'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'GajiGaji.urls'
+ROOT_URLCONF = 'Gaji.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'GajiGaji.wsgi.application'
+WSGI_APPLICATION = 'Gaji.wsgi.application'
 
 
 # Database
