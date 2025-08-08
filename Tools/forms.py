b27814c1358db1c702c9c51363126f2fb97be745
ysh-0205 <ysh_0205@naver.com>
2025-08-08 18:23:53 +0900

    GajiGaji

--- Tools/forms.py
+++ Tools/forms.py
@@ -4,7 +4,7 @@ from Tools.models import Post , Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title','content','category','uploaded_image']
+        fields = ['title','content','Categorytype','Categorygenre','uploaded_image']
 
 class CommentForm(forms.ModelForm):
     class Meta:
--- Tools/models.py
+++ Tools/models.py
@@ -5,14 +5,14 @@ from django.db import models
 
 class Categorytype(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True)
+    slug = models.CharField(unique=True)
 
     def __str__(self):
         return self.name
 
 class Categorygenre(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True)
+    slug = models.CharField(unique=True)
 
     def __str__(self):
         return self.name
